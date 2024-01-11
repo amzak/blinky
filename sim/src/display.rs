cf82e5217aeab57f339d41ebf37781baef15ce85
Andrey Zak <amzak.only@gmail.com>
2024-01-11 20:53:37 +0200

    added rendering simulator

---
+++ sim/src/display.rs
@@ -0,0 +1,85 @@
+use std::{convert::Infallible, io::Seek};
+
+use blinky_shared::display_interface::ClockDisplayInterface;
+use embedded_graphics::{
+    draw_target::DrawTarget,
+    geometry::Size,
+    pixelcolor::{Rgb565, RgbColor},
+};
+use embedded_graphics_framebuf::FrameBuf;
+use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+
+use std::fmt::Debug;
+
+pub struct SimDisplay {
+    buffer: Box<[Rgb565]>,
+    display: SimulatorDisplay<Rgb565>,
+    window: Window,
+}
+
+impl ClockDisplayInterface for SimDisplay {
+    type Error = Infallible;
+
+    type ColorModel = Rgb565;
+
+    type FrameBuffer<'b> =
+        FrameBuf<Self::ColorModel, &'b mut [Self::ColorModel; Self::FRAME_BUFFER_SIZE]>;
+
+    const FRAME_BUFFER_SIDE: usize = 240;
+
+    const FRAME_BUFFER_SIZE: usize = Self::FRAME_BUFFER_SIDE * Self::FRAME_BUFFER_SIDE;
+
+    fn create() -> Self {
+        let buffer = Self::prepare_frame_buf();
+        let display = SimulatorDisplay::<Rgb565>::new(Size::new(
+            Self::FRAME_BUFFER_SIDE as u32,
+            Self::FRAME_BUFFER_SIDE as u32,
+        ));
+
+        let output_settings = OutputSettingsBuilder::new()
+            .scale(3)
+            .pixel_spacing(1)
+            //.theme(BinaryColorTheme::OledBlue)
+            .build();
+
+        let mut window = Window::new("Hello World", &output_settings);
+
+        SimDisplay {
+            buffer,
+            display,
+            window,
+        }
+    }
+
+    fn render<'b, 'a: 'b>(
+        &'a mut self,
+        func: impl FnOnce(Self::FrameBuffer<'b>) -> Self::FrameBuffer<'b>,
+    ) {
+        let data = self.buffer.as_mut();
+
+        let buf: &'b mut [Self::ColorModel; Self::FRAME_BUFFER_SIZE] = data.try_into().unwrap();
+
+        let mut frame = FrameBuf::new(buf, Self::FRAME_BUFFER_SIDE, Self::FRAME_BUFFER_SIDE);
+
+        frame.clear(Rgb565::BLACK).unwrap();
+        frame = func(frame);
+
+        let t = frame.into_iter();
+
+        self.display.draw_iter(t).unwrap();
+
+        self.window.update(&self.display);
+    }
+}
+
+impl SimDisplay {
+    fn prepare_frame_buf<TColor: RgbColor + Debug>() -> Box<[TColor]> {
+        let mut v = Vec::<TColor>::with_capacity(Self::FRAME_BUFFER_SIZE);
+        for _ in 0..v.capacity() {
+            v.push_within_capacity(TColor::BLACK).unwrap();
+        }
+
+        let buffer = v.into_boxed_slice();
+        buffer
+    }
+}
--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,3 +1,8 @@
+#![feature(vec_push_within_capacity)]
+
+use blinky_shared::events::Events;
+use blinky_shared::{commands::Commands, modules::renderer::Renderer};
+use display::SimDisplay;
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X9, MonoTextStyle},
     pixelcolor::Rgb565,
@@ -6,47 +11,60 @@ use embedded_graphics::{
     text::Text,
 };
 use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+use time::OffsetDateTime;
+use tokio::join;
 use tokio::sync::broadcast;
-use blinky_shared::commands::Commands;
-use blinky_shared::events::Events;
+use tokio::time::{sleep, Duration};
+
+mod display;
 
 extern crate blinky_shared;
 
-fn main() -> Result<(), core::convert::Infallible> {
-    let mut display = SimulatorDisplay::<Rgb565>::new(Size::new(240, 240));
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_time()
+        .worker_threads(2)
+        //.thread_stack_size(30 * 1024)
+        .build()?;
 
-    let line_style = PrimitiveStyle::with_stroke(Rgb565::WHITE, 1);
-    let text_style = MonoTextStyle::new(&FONT_6X9, Rgb565::WHITE);
+    rt.block_on(async { main_async().await })?;
+    Ok(())
+}
 
+async fn main_async() -> Result<(), Box<dyn std::error::Error>> {
     let (commands_sender, _) = broadcast::channel::<Commands>(32);
     let (events_sender, _) = broadcast::channel::<Events>(32);
 
-    //Renderer::start::<>()
+    let commands_renderer = commands_sender.clone();
+    let events_renderer = events_sender.clone();
 
-    Circle::new(Point::new(72, 8), 48)
-        .into_styled(line_style)
-        .draw(&mut display)?;
+    let renderer_task = tokio::spawn(async move {
+        Renderer::<SimDisplay>::start(commands_renderer, events_renderer).await;
+    });
 
-    Line::new(Point::new(48, 16), Point::new(8, 16))
-        .into_styled(line_style)
-        .draw(&mut display)?;
+    let commands_input = commands_sender.clone();
 
-    Line::new(Point::new(48, 16), Point::new(64, 32))
-        .into_styled(line_style)
-        .draw(&mut display)?;
+    tokio::task::spawn_blocking(move || {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
 
-    Rectangle::new(Point::new(79, 15), Size::new(34, 34))
-        .into_styled(line_style)
-        .draw(&mut display)?;
+        commands_input.send(Commands::StartDeepSleep).unwrap();
+    });
 
-    Text::new("Hello World!", Point::new(5, 5), text_style).draw(&mut display)?;
+    let startup_sequence = tokio::spawn(async move {
+        commands_sender.send(Commands::ResumeRendering).unwrap();
+        events_sender.send(Events::BatteryLevel(80)).unwrap();
+        events_sender.send(Events::InSync(false)).unwrap();
+        events_sender.send(Events::Temperature(20.0)).unwrap();
 
-    let output_settings = OutputSettingsBuilder::new()
-        .scale(3)
-        .pixel_spacing(1)
-        //.theme(BinaryColorTheme::OledBlue)
-        .build();
-    Window::new("Hello World", &output_settings).show_static(&display);
+        loop {
+            let time_utc = OffsetDateTime::now_utc();
+            events_sender.send(Events::TimeNow(time_utc)).unwrap();
+            sleep(Duration::from_millis(1000)).await;
+        }
+    });
+
+    join!(renderer_task, startup_sequence);
 
     Ok(())
 }
